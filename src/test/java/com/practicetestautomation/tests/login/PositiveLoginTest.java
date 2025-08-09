f4598f379061a97107d42b7f4f18d67c71741f81
Hasith Munasinghe <hasithmu@lbfinance.local>
2025-08-09 23:36:16 +0530

    positive login test

--- src/test/java/com/practicetestautomation/tests/login/PositiveLoginTest.java
+++ src/test/java/com/practicetestautomation/tests/login/PositiveLoginTest.java
@@ -1,13 +1,39 @@
 package com.practicetestautomation.tests.login;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 public class PositiveLoginTest {
     public void TestLoginFunctionality(){
         //Open page
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
         //Type username student into Username field
+        WebElement InputUserName=driver.findElement(By.id("username"));
+        InputUserName.sendKeys("student");
         //Type password Password123 into Password field
+        WebElement InputPassword= driver.findElement(By.id("password"));
+        InputPassword.sendKeys("Password123");
         //Push Submit button
+        WebElement SubmitButton= driver.findElement(By.id("submit"));
+        SubmitButton.click();
         //Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+
+        String ExpectedUrl="https://lbfinance.udemy.com/course/selenium-for-beginners/learn/lecture/45915489#notes";
+        String ActualURL= driver.getCurrentUrl();
+
+        Assert.assertEquals(ActualURL, ExpectedUrl);//first Actual  then Expected
         //Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+        String ExpectedMessage="Congratulations student. You successfully logged in!";
+        String pageSource= driver.getPageSource();
+        Assert.assertTrue(pageSource.contains(ExpectedMessage));
         //Verify button Log out is displayed on the new page
+        WebElement logOutButton= driver.findElement(By.linkText("Log out"));
+        Assert.assertTrue(logOutButton.isDisplayed());
+
+        driver.quit();
     }
 }
